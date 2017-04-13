2f1bbbf683bbeb521758744b1af99e0b011423bc
Dan Jacob and Jesse Hoare Evans <dan+jesse@>
2017-04-13 14:34:00 -0700

    remove space listner

--- js/scripts.js
+++ js/scripts.js
@@ -318,9 +318,9 @@ $(document).ready(function() {
     else if (e.keyCode == 40) { //down arrow
       tetrisBoardObj.moveDown();
     }
-    else if (e.keyCode == 32) {
-      stopDrop()
-    } //space bar
+    // else if (e.keyCode == 32) {
+    //   stopDrop()
+    // } //space bar
     //   while (activePiece.testOpen !== false) {
     //     activePiece.moveDown();
     //   }
